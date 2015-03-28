e53dcbda68159d6e9d4337ca2c7c69fc2a6a439f
AfterLogic Support <support@afterlogic.com>
2015-03-28 20:42:52 +0300

    Deleted v7.4 files

--- adminpanel/core/templates/login-warning.php
+++
@@ -1,3 +0,0 @@
-<div>
-	<?php $this->Data->PrintValue('TxtDesc'); ?>
-</div>
--- adminpanel/modules/common/templates/social-header.php
+++
@@ -1,7 +0,0 @@
-<tr>
-	<td valign="top" colspan="2">
-		<b><?php echo CApi::I18N('ADMIN_PANEL/HELPDESK_SOCIAL'); ?></b>
-		<br />
-		<br />
-	</td>
-</tr>
